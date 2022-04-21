5c58b1a65308e5ed9d38ae32387d9f2ee0d85062
ghost12328 <alexdo798@gmail.com>
2022-04-20 23:48:22 -0400

    Level and move

---
+++ Gamer project/Assets/Script/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float mSpeed;  
+
+    public float speed = 12f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mSpeed = 7f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
