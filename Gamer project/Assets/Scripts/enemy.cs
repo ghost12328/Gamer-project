2326cdac1dccba469efd7f7983111e4a3e5e7c69
EMelo121 <emilymelo121@gmail.com>
2022-05-10 17:24:35 -0400

    VR hand tracking, VR UI, terrain and more
    made it so that the vr hands in game track with the controllers, made the canvas and UI visible on the VR headset, resized skeletons to look a bit more natural, completed the terrain and decoration for area 1 of the game, fixed the walls that were blocking the gate to area 3

--- Gamer project/Assets/Scripts/enemy.cs
+++ Gamer project/Assets/Scripts/enemy.cs
@@ -11,7 +11,7 @@ public class enemy : MonoBehaviour
     private Animator animator;
     public float speed = .02f;
     public int enemyHp;
-    private int enemy;
+    private int enemyLeft;
     // Start is called before the first frame update
     void Start()
     {
